162071cfd85bccfada45c42af16d73348ac93ccc
mulungijoytendo <mtjendo@gmail.com>
2026-03-05 13:58:39 +0300

    fix render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning/" : "",
   output: "export",
   images: {
     unoptimized: true,
